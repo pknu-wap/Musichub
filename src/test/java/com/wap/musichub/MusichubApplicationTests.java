249311822f1fbae68c217bd39d0e4503e90dc58b
yoon6 <yoonda5898@gmail.com>
2021-05-12 00:44:09 +0900

    Set the basic configurations.

---
+++ src/test/java/com/wap/musichub/MusichubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wap.musichub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusichubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
