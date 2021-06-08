972f9769b3ce8cb8dbedda06341e9c02dc0f31f8
Daeyoon <yoonda5898@gmail.com>
2021-06-09 03:00:22 +0900

    live reload
    이미지 업로드 후, 웹 서버를 다시 시작해야만 반영이 됨.
    =>https://bottom-to-top.tistory.com/39 보고 해결.
    
    다른 좋은 방법이 있을 거 같음.

---
+++ src/main/java/com/wap/musichub/config/WebConfiguration.java
@@ -0,0 +1,37 @@
+package com.wap.musichub.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class WebConfiguration {
+
+    @Profile("prod")
+    @Configuration
+    public static class ProdMvcConfiguration implements WebMvcConfigurer {
+
+        @Override
+        public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/**")
+                    .addResourceLocations("classpath:/templates/", "classpath:/static/")
+                    .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
+        }
+    }
+
+    @Profile("local")
+    @Configuration
+    public static class LocalMvcConfiguration implements WebMvcConfigurer {
+
+        @Override
+        public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/**")
+                    .addResourceLocations("file:src/main/resources/templates/", "file:src/main/resources/static/");
+        }
+    }
+
+}
